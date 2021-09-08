61d4e2b638101238db78b84056afaa7568459df9
Tainã Luiz <tluuiiz@gmail.com>
2021-09-08 19:38:21 -0300

    Projeto criado

---
+++ src/main/java/com/github/tainaluiz/workshopmongo/WorkshopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.github.tainaluiz.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongoApplication.class, args);
+	}
+
+}
